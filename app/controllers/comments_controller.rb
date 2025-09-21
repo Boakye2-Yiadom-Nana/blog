93d8a16851d0ba59049c244a237703bb54d8f087
Boakye2-Yiadom-Nana <boakyeyiadomnana99@gmail.com>
2025-09-21 20:49:25 +0000

    Blog page

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,30 @@
+class CommentsController < ApplicationController
+     before_action :set_post
+
+     def create
+        @comment= @post.comments.build(comment_params)
+     
+     if @comment.save
+        redirect_to @post, notice: 'Comment was successfully added. '
+     
+     else
+        redirect_to @post, notice: 'Error adding comment.'
+     end    
+    end
+     
+     def destroy
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        redirect_to @post, notice: 'Comment deleted'
+     end
+
+     private
+
+     def set_post
+        @post = Post.find(params[:post_id])
+     end
+
+     def comment_params
+        params.require(:comment).permit(:name, :email, :content)
+     end
+end
